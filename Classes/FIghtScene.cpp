510630d2dda2420027b5dd4420c2d7c616202a21
nanmuyao <1558249222@qq.com>
2016-03-19 19:33:54 +0800

    战斗场景
    战斗场景

--- Classes/FIghtScene.cpp
+++ Classes/FIghtScene.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "FIghtScene.hpp"
+
+Scene* FightScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = FightScene::create();
+    
+    // add layer as a child to scene
+    scene->addChild(layer);
+    
+    // return the scene
+    return scene;
+}
+
--- Classes/FIghtScene.hpp
+++ Classes/FIghtScene.hpp
@@ -8,7 +8,22 @@
 
 #ifndef FIghtScene_hpp
 #define FIghtScene_hpp
-
+#include "cocos2d.h"
 #include <stdio.h>
+
+using namespace cocos2d;
+
+class FightScene : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+    
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(FightScene);
+};
 
 #endif /* FIghtScene_hpp */
